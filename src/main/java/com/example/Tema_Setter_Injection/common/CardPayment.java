6eb8cabdc78acff18dca768a89491214da0573f0
Florea Alina <floreaalina340@gmail.com>
2026-05-27 11:21:44 +0300

    Implement class CardPayment

---
+++ src/main/java/com/example/Tema_Setter_Injection/common/CardPayment.java
@@ -0,0 +1,10 @@
+package com.example.Tema_Setter_Injection.common;
+
+public class CardPayment implements PaymentService{
+
+    @Override
+    public String processPayment(double amount) {
+        return "Plata cu cardul " + amount + "ron a fost procesata";
+    }
+
+}
