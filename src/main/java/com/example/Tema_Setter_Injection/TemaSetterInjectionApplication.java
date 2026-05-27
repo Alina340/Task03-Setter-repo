393f536cdb7d9bba8b2b99d0bae58780b8b01e0e
Florea Alina <floreaalina340@gmail.com>
2026-05-27 10:56:40 +0300

    Initial commit

---
+++ src/main/java/com/example/Tema_Setter_Injection/TemaSetterInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tema_Setter_Injection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemaSetterInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemaSetterInjectionApplication.class, args);
+	}
+
+}
